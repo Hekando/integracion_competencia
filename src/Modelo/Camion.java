93840bb39a3a6e0ef195df67c3d385f2f866cdc9
paula-aguilera <paula.uta@gmail.com>
2026-04-06 16:02:45 -0400

    Update Camion.java

--- src/Modelo/Camion.java
+++ src/Modelo/Camion.java
@@ -1,4 +1,35 @@
 package Modelo;
 
-public class Camiones {
+public class Camion {
+
+    private int idCamion;
+    private String patente;
+    private String marca;
+    private String modelo;
+    private int kilometraje;
+
+    public Camion() {}
+
+    public Camion(int idCamion, String patente, String marca, String modelo, int kilometraje) {
+        this.idCamion = idCamion;
+        this.patente = patente;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.kilometraje = kilometraje;
+    }
+
+    public int getIdCamion() { return idCamion; }
+    public void setIdCamion(int idCamion) { this.idCamion = idCamion; }
+
+    public String getPatente() { return patente; }
+    public void setPatente(String patente) { this.patente = patente; }
+
+    public String getMarca() { return marca; }
+    public void setMarca(String marca) { this.marca = marca; }
+
+    public String getModelo() { return modelo; }
+    public void setModelo(String modelo) { this.modelo = modelo; }
+
+    public int getKilometraje() { return kilometraje; }
+    public void setKilometraje(int kilometraje) { this.kilometraje = kilometraje; }
 }
